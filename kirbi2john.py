fdfc3f222c480cb42bf8927c763ff01117de7127
Touhid M Shaikh <touhidshaikh22@gmail.com>
2020-11-19 22:43:40 +0530

    updated kirbi2john.py
    due to "/usr/bin/env python -tt" shebang was not running to fix this I have removed the '-tt'  from shebeng

--- kirbi2john.py
+++ kirbi2john.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3 -tt
+#!/usr/bin/env python3
 # Based on the Kerberoast script from Tim Medin to extract the Kerberos tickets
 # from a kirbi file.
 # Modification to parse them into the JTR-format by Michael Kramer (SySS GmbH)
